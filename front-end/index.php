252f97fcac31782669a9be761418792aa8f79305
LucieLecouturier <LucieGitHub@gmail.com>
2022-04-28 16:56:38 +0200

    delete equals from the merge

--- front-end/index.php
+++ front-end/index.php
@@ -1,14 +1,14 @@
 <?php
 
 $Json = file_get_contents('./wordingUtils/en.json');
- 
+
 $enJson = json_decode($Json, true);
 
 // Prevent the user from going on non-existing pages
 session_start();
-require_once __DIR__.'/../back-end/db.php'; 
+require_once __DIR__ . '/../back-end/db.php';
 $page_title = 'Unknown page?';
-$existing_pages = ['Home', 'Explorer', 'Subscriptions' , 'Library', 'History', 'LikedVideo', 'WatchLater'];
+$existing_pages = ['Home', 'Explorer', 'Subscriptions', 'Library', 'History', 'LikedVideo', 'WatchLater'];
 
 if (empty($_GET['name']) == false) {
     if (in_array($_GET['name'], $existing_pages)) {
@@ -16,32 +16,30 @@ if (empty($_GET['name']) == false) {
     } else {
         $pageName = "404";
     }
-}else {
+} else {
     $pageName = "Home";
 }
 
-$request = explode('?',$_SERVER['REQUEST_URI']);
+$request = explode('?', $_SERVER['REQUEST_URI']);
 
 switch ($request[0]) {
-     /* case '/':
+        /* case '/':
         require_once __DIR__ . '/views/Profile/home.php';
         break; */
     case '/login':
         require_once __DIR__ . '/views/Profile/login.php';
-        break; 
+        break;
     case '/register':
         require_once __DIR__ . '/views/Profile/register.php';
         break;
-    /*  case '/deconnexion':
+        /*  case '/deconnexion':
         require_once __DIR__. '/action/disconnect.php';
         break; */
     case '/profils':
         require_once __DIR__ . '/views/Profile/profils.php';
-        break; 
-
+        break;
 }
 
-=======
-include __DIR__ . '/views/Timeline/' . $pageName. '.php'; // $page 
+include __DIR__ . '/views/Timeline/' . $pageName . '.php'; // $page 
 include __DIR__ . "/partials/Menu.php"; // $menu
 require_once __DIR__ . '/template.php';
