04043a539e08b8897b804616e565ab580418c7a2
elimf <elim.florvil@gmail.com>
2022-04-28 12:15:51 +0200

    pour Eddy

--- front-end/template.php
+++ front-end/template.php
@@ -3,18 +3,21 @@
 
 <head>
     <meta charset="UTF-8">
-    <title><?= $enJson['global']['title'] ?></title>
+    <title><?= $enJson['global']['title'] ?> | <?= $title ?></title>
+
     <link rel="stylesheet" href="./assets/css/global.css">
+        <link rel="stylesheet" href="./assets/css/navbar.css">
 </head>
 
 <body>
-    <!-- Put all components here -->
+    <?php require_once __DIR__ . "/../front-end/partials/navbar.php" ?>
     <div class="content">
 
-        <a href="/register">bouge</a>
+    <?php echo $content; ?>
 
     </div>
     <!-- Add js library and script here -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <!-- <script src="./assets/js/script-menu.js"></script> -->
 </body>
 
