1380dffb629435f793a29ec94f2bfac2799ad2cf
LucieLecouturier <LucieGitHub@gmail.com>
2022-04-29 16:21:50 +0200

    strange bug for Jerance :)

--- front-end/views/pages/Explorer.php
+++ front-end/views/pages/Explorer.php
@@ -2,61 +2,68 @@
 $page = "Explorer"; ?>
 
 <div class="explorer-container">
-    <div class="categories-grid">
-        <div class="category-explorer">
-            <div class="category-content">
-                <img src="/front-end/assets/img/Menu-svg/compass.svg" alt="logo-category">
-                <p class="category-explorer-title">Trending</p>
+    <div class="explorer-categories">
+        <div class="categories-grid">
+            <div class="category-explorer">
+                <div class="category-content">
+                    <img src="/front-end/assets/img/Menu-svg/compass.svg" alt="logo-category">
+                    <p class="category-explorer-title">Trending</p>
+                </div>
+            </div>
+            <div class="category-explorer">
+                <div class="category-content">
+                    <img src="/front-end/assets/img/Menu-svg/compass.svg" alt="logo-category">
+                    <p class="category-explorer-title">Trending</p>
+                </div>
+            </div>
+            <div class="category-explorer">
+                <div class="category-content">
+                    <img src="/front-end/assets/img/Menu-svg/compass.svg" alt="logo-category">
+                    <p class="category-explorer-title">Trending</p>
+                </div>
+            </div>
+            <div class="category-explorer">
+                <div class="category-content">
+                    <img src="/front-end/assets/img/Menu-svg/compass.svg" alt="logo-category">
+                    <p class="category-explorer-title">Trending</p>
+                </div>
+            </div>
+            <div class="category-explorer">
+                <div class="category-content">
+                    <img src="/front-end/assets/img/Menu-svg/compass.svg" alt="logo-category">
+                    <p class="category-explorer-title">Trending</p>
+                </div>
+            </div>
+            <div class="category-explorer">
+                <div class="category-content">
+                    <img src="/front-end/assets/img/Menu-svg/compass.svg" alt="logo-category">
+                    <p class="category-explorer-title">Trending</p>
+                </div>
+            </div>
+            <div class="category-explorer">
+                <div class="category-content">
+                    <img src="/front-end/assets/img/Menu-svg/compass.svg" alt="logo-category">
+                    <p class="category-explorer-title">Trending</p>
+                </div>
+            </div>
+            <div class="category-explorer">
+                <div class="category-content">
+                    <img src="/front-end/assets/img/Menu-svg/compass.svg" alt="logo-category">
+                    <p class="category-explorer-title">Trending</p>
+                </div>
+            </div>
+            <div class="category-explorer">
+                <div class="category-content">
+                    <img src="/front-end/assets/img/Menu-svg/compass.svg" alt="logo-category">
+                    <p class="category-explorer-title">Trending</p>
+                </div>
             </div>
         </div>
-        <div class="category-explorer">
-            <div class="category-content">
-                <img src="/front-end/assets/img/Menu-svg/compass.svg" alt="logo-category">
-                <p class="category-explorer-title">Trending</p>
-            </div>
-        </div>
-        <div class="category-explorer">
-            <div class="category-content">
-                <img src="/front-end/assets/img/Menu-svg/compass.svg" alt="logo-category">
-                <p class="category-explorer-title">Trending</p>
-            </div>
-        </div>
-        <div class="category-explorer">
-            <div class="category-content">
-                <img src="/front-end/assets/img/Menu-svg/compass.svg" alt="logo-category">
-                <p class="category-explorer-title">Trending</p>
-            </div>
-        </div>
-        <div class="category-explorer">
-            <div class="category-content">
-                <img src="/front-end/assets/img/Menu-svg/compass.svg" alt="logo-category">
-                <p class="category-explorer-title">Trending</p>
-            </div>
-        </div>
-        <div class="category-explorer">
-            <div class="category-content">
-                <img src="/front-end/assets/img/Menu-svg/compass.svg" alt="logo-category">
-                <p class="category-explorer-title">Trending</p>
-            </div>
-        </div>
-        <div class="category-explorer">
-            <div class="category-content">
-                <img src="/front-end/assets/img/Menu-svg/compass.svg" alt="logo-category">
-                <p class="category-explorer-title">Trending</p>
-            </div>
-        </div>
-        <div class="category-explorer">
-            <div class="category-content">
-                <img src="/front-end/assets/img/Menu-svg/compass.svg" alt="logo-category">
-                <p class="category-explorer-title">Trending</p>
-            </div>
-        </div>
-        <div class="category-explorer">
-            <div class="category-content">
-                <img src="/front-end/assets/img/Menu-svg/compass.svg" alt="logo-category">
-                <p class="category-explorer-title">Trending</p>
-            </div>
-        </div>
+
+    </div>
+
+    <div class="creator-rise">
+        <div class="creator-rise-content"></div>
     </div>
 
 </div>
