b4fa58f8233740f21c8d4fbdc1bf65a9ed002cdf
Celiian <celian.opigez@gmail.com>
2022-04-29 13:23:40 +0200

    fix

--- template.php
+++ template.php
@@ -16,8 +16,7 @@
     <div class="content">
 
       <?= $upload; ?>
-      
-      
+
     </div>
     <!-- Add js library and script here -->
     <script src="https://code.jquery.com/jquery-3.6.0.slim.min.js" integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI=" crossorigin="anonymous"></script>
