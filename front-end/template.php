0da85e484be2a02cdb5eabbb94e39ab911afe7aa
Killian Vendewinkele <killianvendewinkele@MacBook-Pro-de-admin.local>
2022-04-29 10:51:28 +0200

    fix/search id/class have been modified without - but with Uppercase

--- front-end/template.php
+++ front-end/template.php
@@ -2,8 +2,12 @@
 <html lang="en">
 
 <head>
+    <?php 
+    $en_json = file_get_contents('wordingUtils/en.json'); 
+    $decoded_en_json = json_decode($en_json, true);
+    ?>
     <meta charset="UTF-8">
-    <title><?= $enJson['global']['title'] ?></title>
+    <title><?= $decoded_en_json['global']['title'] ?></title>
     <link rel="stylesheet" href="./assets/css/global.css">
     <link rel="stylesheet" href="./assets/css/SearchBar.css">
     <link rel="stylesheet" href="./assets/css/SearchPage.css">
@@ -12,12 +16,10 @@
 <body>
     <!-- Put all components here -->
     <div class="content">
-        
         <?php require_once "views/SearchBar/Searchbar.php" ?>
         <?php require_once "views/SearchBar/SearchPage.php" ?>
-
-
     </div>
+
     <!-- Add js library and script here -->
     <script src="./assets/js/SearchPage.js"></script>
     <script src="./assets/js/script-menu.js"></script>
--- front-end/views/SearchBar/SearchBar.php
+++ front-end/views/SearchBar/SearchBar.php
@@ -1,32 +1,56 @@
 <!DOCTYPE html>
-<html lang="en">
 <head>
+    <html lang="en">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <?php
 
-    ?>
+if(isset($_GET["searchbar_content"])){
+    $_filters["srch_cntnt"] = $_GET["searchbar_content"];
+    echo($_filters["srch_cntnt"]);
+}
+if(isset($_GET["filter_upload_date"])){
+    $_filters["up_date"] = $_GET["filter_upload_date"];
+    echo($_filters["up_date"]);
+}else{
+    $_filters["up_date"] = "";
+}
+if(isset($_GET["filter_type"])){
+    $_filters["type"] = $_GET["filter_type"];
+    echo($_filters["type"]);
+}else{
+    $_filters["type"] = "";
+}
+if(isset($_GET["filter_order_by"])){
+    $_filters["ord_by"] = $_GET["filter_order_by"];
+    echo($_filters["ord_by"]);
+}else{
+    $_filters["ord_by"] = "";
+}
+$array = ['SearchBarContent' => $_filters["srch_cntnt"], ];
+echo json_encode($array, JSON_PRETTY_PRINT);
+?>
 </head>
 <body>
     <!-- The entire searchbar-->
-    <div id="search-bar">
+    <div id="searchBar">
         <div id="searchbar" class="row">
             <!-- The form where we get the result from the url -->
-            <form method="GET" class="nav-search-box" action="">
+            <form method="GET" class="navSearchBox" action="/front-end/template.php">
                 <input
                 type="text"
-                name="searchbar-content"
-                id="searchbar-content"
+                name="searchbar_content"
+                id="searchbarContent"
                 placeholder="Search Pradish"
                 />
-                <div class="search-button">
-                <img class="search-icon"
+                <button type="submit" class="searchButton">
+                <img class="searchIcon"
                     src="./assets/img/magnifying-glass-solid.svg"
                     alt="magnifying glass" @click="fonctionDeRecherche"
                 />
-                </div>
+                </button>
             </form>
         </div>
     </div>
