9c7331702763d1c9b87b6d24574ebfe82bfc352c
LucieLecouturier <LucieGitHub@gmail.com>
2022-04-29 11:01:26 +0200

    Fix Menu and add responsive page content

--- template.php
+++ template.php
@@ -11,19 +11,25 @@
     <link rel="stylesheet" href="./front-end/partials/Menu.css">
     <link rel="stylesheet" href="./front-end/views/Timeline/Home.css">
 </head>
-<body>
-   
-    <div class="content">
 
-        <?= $navBar; ?>
-        <?= $menu; ?>
-       <?= $pageName ?>
-      
+<body>
+
+    <?= $navBar; ?>
+    <div class="content">
+        <div class="menus">
+            <?= $menu; ?>
+        </div>
+        <div class="page-content">
+            <div class="page">
+                <?= $pageName ?>
+            </div>
+        </div>
     </div>
     <!-- Add js library and script here -->
-    <script src="https://code.jquery.com/jquery-3.6.0.slim.min.js" integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.slim.min.js"
+        integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI=" crossorigin="anonymous"></script>
     <script src="./front-end/assets/js/script-menu.js"></script>
 
-  </body>
+</body>
 
 </html>
