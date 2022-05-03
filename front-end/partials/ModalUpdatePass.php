2f362f9e2df042ec5bd8d65f9ad881cc874899d1
elimf <elim.florvil@gmail.com>
2022-05-03 11:43:16 +0200

    Feature/CSS MODAL UPDATE PASSWORD

--- front-end/partials/ModalUpdatePass.php
+++ front-end/partials/ModalUpdatePass.php
@@ -1,81 +1,62 @@
  <button class="buttonModale" onclick="document.getElementById('password').style.display='block'">Update password</button>
- 
-         <div id="password" class="modal" <?php if($_SESSION['process'] == true) echo 'style = "display : block";'?>>
-            <span onclick="document.getElementById('password').style.display='none'" class="close" title="Close Modal">
-         </span>
-            <form class="modal-content" enctype="multipart/form-data"  id="form" method="post" action="/back-end/actions/UpdatePassword.php">
-            <div class="close-form">
-         <a href="/index.php?name=Profile"><img src="./front-end/assets/img/Logo/x.svg" alt=""></a>
 
-      </div>
-               <div class="wrapper-part-profile">
-               <div>
-                     <label class="label-form" for="password">Old Password</label>
-                     <input
-                        name="old-password"
-                        class="input--register"
-                        type="password"
-                        placeholder=" Your old password "
-                        id="old-password"
-                        />
-                  </div>
+ <div id="password" class="modal" <?php if ($_SESSION['process'] == true) echo 'style = "display : block";' ?>>
+    <span onclick="document.getElementById('password').style.display='none'" class="close" title="Close Modal">
+    </span>
+    <form class="modal-content" enctype="multipart/form-data" id="form" method="post" action="/back-end/actions/UpdatePassword.php">
+       <div class="close-form">
+          <a onclick="document.getElementById('password').style.display='none'" class="close" title="Close Modal"><img src="./front-end/assets/img/Logo/x.svg" alt=""></a>
 
-               <div>
-                     <label class="label-form" for="password">New Password</label>
-                     <input
-                        name="new-password"
-                        class="input--register"
-                        type="password"
-                        placeholder="New password"
-                        id="new-password"
-                        />
-                  </div>
-                  
-                  <div>
-                     <label class="label-form" for="password">Confirm password</label>
-                     <input
-                        name="confirm-password"
-                        class="input--register"
-                        type="password"
-                        placeholder="Confirm password"
-                        id="confirm-password"
-                        
-                        />
-                  </div>
-               </div>
-               
-               <div class="backtoback">
-                  <button class="btn--register" id="update"><?= $enJson['form']['updatePass']['buttonUpdate'] ?></button>
-               </div>
-            </form>
-         </div>
+       </div>
+       <div class="wrapper-part-profile">
+          <div>
+             <label class="label-form" for="password">Old Password</label>
+             <input name="old-password" class="input--register" type="password" placeholder=" Your old password " id="old-password" />
+          </div>
 
-         <script>
-            var data = [];
+          <div>
+             <label class="label-form" for="password">New Password</label>
+             <input name="new-password" class="input--register" type="password" placeholder="New password" id="new-password" />
+          </div>
+
+          <div>
+             <label class="label-form" for="password">Confirm password</label>
+             <input name="confirm-password" class="input--register" type="password" placeholder="Confirm password" id="confirm-password" />
+          </div>
+       </div>
+
+       <div class="backtoback">
+          <button class="btn--register" id="update"><?= $enJson['form']['updatePass']['buttonUpdate'] ?></button>
+       </div>
+    </form>
+ </div>
+
+ <script>
+    var data = [];
     data.push(document.getElementById('old-password'));
     data.push(document.getElementById('new-password'));
     data.push(document.getElementById('confirm-password'));
     form.addEventListener("keyup", function(e) {
-        for (var i in data) {
+       for (var i in data) {
 
-            if (data[i].value != 0) {
-                data[i].classList.remove("show-error-connection");
-            }
-        }
+          if (data[i].value != 0) {
+             data[i].classList.remove("show-error-connection");
+          }
+       }
     });
-            form.onsubmit = (e) => {
-        for (var i in data) {
-            if (data[i].value == 0) {
-                e.preventDefault();
-                data[i].placeholder = ('Put your ' + data[i].id);
-                data[i].classList.add("show-error-connection");
-            }
-        }
-        setTimeout(() => {$(function() {
-		 toastr.error('<?= $enJson['form']['error']['signup'] ?>', '');
-	});}, 200);
-        
-    }
-         </script>
+    form.onsubmit = (e) => {
+       for (var i in data) {
+          if (data[i].value == 0) {
+             e.preventDefault();
+             data[i].placeholder = ('Put your ' + data[i].id);
+             data[i].classList.add("show-error-connection");
+          }
+       }
+       setTimeout(() => {
+          $(function() {
+             toastr.error('<?= $enJson['form']['error']['signup'] ?>', '');
+          });
+       }, 200);
 
-     
+    }
+ </script>
--- front-end/partials/ModalUpdateUser.php
+++ front-end/partials/ModalUpdateUser.php
@@ -1,60 +1,56 @@
-<button class="buttonModale" onclick="document.getElementById('update--form').style.display='block';" >View advanced settings</button>
+<button class="buttonModale" onclick="document.getElementById('update--form').style.display='block';">View advanced settings</button>
 
 
 <div id="update--form" class="modal">
-    <form class="modal-content" enctype="multipart/form-data"  id="form" method="post" action="/back-end/actions/update_user.php">
-        <div class="close-form">
-            <a href="/index.php?name=Profile"><img src="./front-end/assets/img/Logo/x.svg" alt=""></a>
-</div>
-        <table>
-        <tr>
-                   <td >
-                       <span>Your Username : </span>
-                    </td>
-                    <td>
-                     <span>
-                        <?= $_SESSION['user']['username'] ?> </span>
-                       
-                    </td>
-                    <td> <input name="username" class="input--register" type="text" />
-                    </td>
-                </tr>
-               <tr>
-                   <td >
-                       <span>Your Email : </span>
-                    </td>
-                    <td>
-                     <span>
-                        <?= $_SESSION['user']['email'] ?> </span>
-                       
-                    </td>
-                    <td><input name="email" class="input--register" type="text" placeholder="New email" id="password"/>
-                    </td>
-                </tr>
-                <tr>
-                   <td >
-                       <span>Your Birthday : </span>
-                    </td>
-                    <td>
-                     <span>
-                        <?= $_SESSION['user']['birthday'] ?> </span>
-                       
-                    </td>
-                    <td> <input name="dateBirth" class="input--register" type="date" id="dateofbirthday" />
-                    </td>
-                </tr>
-        </table>
-        
-               <div class="backtoback">
-                  <button class="btn--register" id="update">Update your profile</button>
-               </div>
-            </form>
+   <form class="modal-content" enctype="multipart/form-data" id="form" method="post" action="/back-end/actions/update_user.php">
+      <div class="close-form">
+         <a onclick="document.getElementById('password').style.display='none'" class="close" title="Close Modal"><img src="./front-end/assets/img/Logo/x.svg" alt=""></a>
+      </div>
+      <table>
+         <tr>
+            <td>
+               <span>Your Username : </span>
+            </td>
+            <td>
+               <span>
+                  <?= $_SESSION['user']['username'] ?> </span>
+
+            </td>
+            <td> <input name="username" class="input--register" type="text" />
+            </td>
+         </tr>
+         <tr>
+            <td>
+               <span>Your Email : </span>
+            </td>
+            <td>
+               <span>
+                  <?= $_SESSION['user']['email'] ?> </span>
+
+            </td>
+            <td><input name="email" class="input--register" type="text" placeholder="New email" id="password" />
+            </td>
+         </tr>
+         <tr>
+            <td>
+               <span>Your Birthday : </span>
+            </td>
+            <td>
+               <span>
+                  <?= $_SESSION['user']['birthday'] ?> </span>
+
+            </td>
+            <td> <input name="dateBirth" class="input--register" type="date" id="dateofbirthday" />
+            </td>
+         </tr>
+      </table>
+
+      <div class="backtoback">
+         <button class="btn--register" id="update">Update your profile</button>
+      </div>
+   </form>
 </div>
 <script>
-    
-
-    
-
    $('input[type="file"]').on('change', (e) => {
       let that = e.currentTarget
       if (that.files && that.files[0]) {
@@ -65,8 +61,4 @@
          reader.readAsDataURL(that.files[0])
       }
    })
-
-   
-
 </script>
-
