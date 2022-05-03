f34ae488b764e2de07c630e9e4bdf1e8e6ff7750
Jerance MICHAEL <michael.jerance@gmail.com>
2022-05-03 11:31:51 +0200

    Update MetaTubePremium in en.json

--- front-end/views/pages/MetaTubePremium.php
+++ front-end/views/pages/MetaTubePremium.php
@@ -3,15 +3,21 @@
 <div class="MetaTubePremium-container">
 
     <img src="../../../front-end/assets/img/MetaTubePremium/logov3.svg" alt="MetaTube Premium" srcset="" />
-    <p class="premium-p">MetaTube and MetaTube Music ad-free offline, and in the background</p>
+    <p class="premium-p"><?= $enJson['MetaTubePremium']['title'] ?></p>
 
     <div class="button-container">
-        <button class="btn-free">Try it Free</button>
+        <button class="btn-free"><?= $enJson['MetaTubePremium']['txt-button'] ?></button>
     </div>
-    <h2>2-month free trial • Then €11.99/month</h2>
-    <p>We'll remind you 7 days before your trial ends Recurring billing • Cancel anytime</p>
-    <p>Or save money with a <a href="" class="link-premium">family or student plan</a></p>
-    <a class="link-premium" href="">Restrictions apply. Learn more here.</a>
+    <h2><?= $enJson['MetaTubePremium']['price'] ?></h2>
+    <p><?= $enJson['MetaTubePremium']['remind-text'] ?></p>
+    <p><?= $enJson['MetaTubePremium']['add-price'] ?><a href="" class="link-premium"><?= $enJson['MetaTubePremium']['link-add-price'] ?></a></p>
+    <a class="link-premium" href=""><?= $enJson['MetaTubePremium']['link-premium'] ?></a>
+</div>
+
+<div class="container2">
+    <div class="ad-free">
+        
+    </div>
 </div>
 
 <?php $pageName = ob_get_clean() ?>
