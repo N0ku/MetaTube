a52aa4ada4a13f723d079e8b6964200bf2be7fd2
ThomLam <lamiablethomas@gmail.com>
2022-05-04 17:37:16 +0200

    Feature - Add Selected Category HTML, Development in Stand By for Thomlam - 3Hours

--- front-end/views/pages/MetaCreator/Content.php
+++ front-end/views/pages/MetaCreator/Content.php
@@ -4,9 +4,13 @@
         <h1 id="MetaStudio-title-h1">Content of the channel</h1>
     </div>
     <div class="MetaStudio-separator-rgb"></div>
-
     <div class="MetaStudio-selected-category">
-<h1>ntm</h1>
+        <div class="MetaStudio-selected-video">
+            <h2 id="MetaStudio-selected-video-h2">Videos</h2>
+        </div>
+        <div class="MetaStudio-selected-commentary">
+            <h2 id="MetaStudio-selected-commentary-h2">Commentary</h2>
+        </div>
     </div>
     <div class="MetaStudio-filter-bar">
 <h2>Bite</h2>
