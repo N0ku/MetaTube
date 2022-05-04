c97a980605a5b39ff9b590472309581d17b078fe
Jerance MICHAEL <michael.jerance@gmail.com>
2022-05-04 08:40:32 +0200

    Add routes menu Pages

--- front-end/partials/Menu.php
+++ front-end/partials/Menu.php
@@ -102,43 +102,42 @@
             <strong class="title-others">
                 <?= $enJson['menu']['titles']['others'] ?>
             </strong>
-            <a href="index.php?name=MetaTubePremium">
-                <div class="nav-item">
-                    <img src="./front-end/assets/img/Menu-svg/ytb-premium.svg" alt="MetaTube Premium" srcset="" class="nav-logo" />
-                    <p><?= $enJson['menu']['nav-elements']['MetaTube Premium'] ?></p>
-                </div>
-            </a>
+            <div class="nav-item">
+                <img src="./front-end/assets/img/Menu-svg/ytb-premium.svg" alt="MetaTube Premium" srcset="" class="nav-logo" />
+                <p><a href="index.php?name=MetaTubePremium"><?= $enJson['menu']['nav-elements']['MetaTube Premium'] ?></a></p>
+            </div>
+            
             <div class="nav-item">
                 <img src="./front-end/assets/img/Menu-svg/movies.svg" alt="Movies and Shows" srcset="" class="nav-logo" />
-                <p><a href="https://www.youtube.com/feed/storefront?bp=ogUCKAI%3D"><?= $enJson['menu']['nav-elements']['Movies and Shows'] ?></a></p>
+                <p><a href="index.php?name=MoviesShows"><?= $enJson['menu']['nav-elements']['Movies and Shows'] ?></a></p>
             </div>
             <div class="nav-item">
                 <img src="./front-end/assets/img/Menu-svg/gaming-logo.svg" alt="Gaming" srcset="" class="nav-logo" />
-                <p><a href="https://www.youtube.com/gaming"><?= $enJson['menu']['nav-elements']['Gaming'] ?></a></p>
+                <p><a href="index.php?name=Gaming"><?= $enJson['menu']['nav-elements']['Gaming'] ?></a></p>
             </div>
             <div class="nav-item">
                 <img src="./front-end/assets/img/Menu-svg/live-logo.svg" alt="Live" srcset="" class="nav-logo" />
-                <p><a href="https://www.youtube.com/channel/UC4R8DWoMoI7CAwX8_LjQHig"><?= $enJson['menu']['nav-elements']['Live'] ?></a></p>
+                <p><a href="index.php?name=Live"><?= $enJson['menu']['nav-elements']['Live'] ?></a></p>
             </div>
             <div class="nav-item">
                 <img src="./front-end/assets/img/Menu-svg/music-logo.svg" alt="Music" srcset="" class="nav-logo" />
-                <p><a href="https://www.youtube.com/channel/UC-9-kyTW8ZkZNDHQJ6FgpwQ"><?= $enJson['menu']['nav-elements']['Music'] ?></a></p>
+                <p><a href="index.php?name=Music"><?= $enJson['menu']['nav-elements']['Music'] ?></a></p>
             </div>
             <div class="nav-item">
                 <img src="./front-end/assets/img/Menu-svg/sports-logo.svg" alt="Sports" srcset="" class="nav-logo" />
-                <p><a href="https://www.youtube.com/channel/UCEgdi0XIXXZ-qJOFPf4JSKw"><?= $enJson['menu']['nav-elements']['Sports'] ?></a></p>
+                <p><a href="index.php?name=Sport"><?= $enJson['menu']['nav-elements']['Sports'] ?></a></p>
             </div>
             <div class="nav-item">
                 <img src="./front-end/assets/img/Menu-svg/learning-logo.svg" alt="Learning" srcset="" class="nav-logo" />
-                <p><a href="https://www.youtube.com/channel/UCtFRv9O2AHqOZjjynzrv-xg"><?= $enJson['menu']['nav-elements']['Learning'] ?></a></p>
+                <p><a href="index.php?name=Learning"><?= $enJson['menu']['nav-elements']['Learning'] ?></a></p>
             </div>
             <div class="nav-item">
                 <img src="./front-end/assets/img/Menu-svg/news-logo.svg" alt="News" srcset="" class="nav-logo" />
-                <p><a href="https://www.youtube.com/channel/UCYfdidRxbB8Qhf0Nx7ioOYw"><?= $enJson['menu']['nav-elements']['News'] ?></a></p>
+                <p><a href="index.php?name=News"><?= $enJson['menu']['nav-elements']['News'] ?></a></p>
             </div>
             <div class="nav-item">
                 <img src="./front-end/assets/img/Menu-svg/beauty-fashion-logo.svg" alt="Fashion & Beauty" srcset="" class="nav-logo" />
-                <p><a href="https://www.youtube.com/channel/UCrpQ4p1Ql_hG8rKXIKM1MOQ"><?= $enJson['menu']['nav-elements']['Fashion & Beauty'] ?></a></p>
+                <p><a href="index.php?name=FashionBeauty"><?= $enJson['menu']['nav-elements']['Fashion & Beauty'] ?></a></p>
             </div>
             <div class="border"></div>
             <div class="border"></div>
