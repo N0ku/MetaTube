6f27887c469c3c4ab71a42e8fce33dde69a8ea4a
ThomLam <lamiablethomas@gmail.com>
2022-05-05 00:20:07 +0200

    Feature - Finish template and CSS MetaStudio

--- front-end/views/pages/MetaCreator/Content.php
+++ front-end/views/pages/MetaCreator/Content.php
@@ -16,16 +16,27 @@
     <h2>FilterBar</h2>
     </div>
     <div class="MetaStudio-user-video-config">
+        <h4 class="MetaStudio-videolist">Video</h4>
+        <h4 class="MetaStudio-visibilitylist">Visibility</h4>
+        <h4 class="MetaStudio-restrictionlist">Restriction</h4>
+        <h4 class="MetaStudio-datelist">Date</h4>
+        <h4 class="MetaStudio-viewlist">View</h4>
+        <h4 class="MetaStudio-numbercommentarylist">Commentary</h4>
+        <h4 class="MetaStudio-likelist">% Like</h4>
+
+
+
+
+    </div>
+
     <div class="MetaStudio-user-video-template">
          <div class="MetaStudo-user-video-container">
          <div class="MetaStudo-user-video-this-video">
             <div class="home-video-content">
             <?php include "./front-end/views/components/templateMetaCreatorVideo.php" ?>
-
             </div>
          </div>
          <div class="MetaStudo-user-video-this-visibility">
-
          </div>
          <div class="MetaStudo-user-video-this-restriction"></div>
          <div class="MetaStudo-user-video-this-date"></div>
@@ -34,7 +45,6 @@
          <div class="MetaStudo-user-video-this-like"></div>
     </div>
 
-    </div>
    
 </div>
 
