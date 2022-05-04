271fe85745fed524ff1547971678712fdbea6bc7
EddyMuzaffar <eddy.muzaffar@outlook.com>
2022-05-04 11:03:37 +0200

    remove die() on action

--- back-end/actions/LoginAction.php
+++ back-end/actions/LoginAction.php
@@ -14,42 +14,44 @@ $query->execute([
 $data = $query->fetch(PDO::FETCH_ASSOC);
 
 
-if( empty($_POST['username']) || empty( $_POST['password'])){
-    //$_SESSION['signup_error']= $enJson['form']['error']['signup'] ;
-    header("Location: /index.php?name=Login ");
-    die();
-}
-
-$password = hash('sha256',$_POST['password']);
-
-
-if ($res["username"] == $data["email"] && $password == $data["password"] ){
-     $_SESSION['user'] = $data;
-     $_SESSION['connect'] = true;
-    $_SESSION['toastr'] = array(
-        'type'      => 'success', 
-        'message' => "Welcome, {$data['username']}",
-        'title'     => ''
-    ); 
-    header('Location: /index.php?name=Home');
+if( !empty($_POST['username']) && !empty( $_POST['password'])){
+    $password = hash('sha256',$_POST['password']);  
+    if ($res["username"] == $data["email"] && $password == $data["password"] ){
+        $_SESSION['user'] = $data;
+        $_SESSION['connect'] = true;
+       $_SESSION['toastr'] = array(
+           'type'      => 'success', 
+           'message' => "Welcome, {$data['username']}",
+           'title'     => ''
+       ); 
+       header('Location: /index.php?name=Home');
+   }else{
+       if($res["username"] != $data["email"]){
+           $_SESSION['toastr'] = array(
+               'type'      => 'error', 
+               'message' => "This email account doesn't exist",
+               'title'     => ''
+           ); 
+       //$_SESSION['signup_error']= $enJson['form']['error']['signup'];
+       header("Location: /index.php?name=Login ");
+       }else{
+           $_SESSION['toastr'] = array(
+               'type'      => 'error', 
+               'message' => "Warning password wrong",
+               'title'     => ''
+           ); 
+   
+       //$_SESSION['signup_error']= $enJson['form']['error']['password'];
+       header("Location: /index.php?name=Login ");}
+   
+   }  
+    
 }else{
-    if($res["username"] != $data["email"]){
-        $_SESSION['toastr'] = array(
-            'type'      => 'error', 
-            'message' => $enJson['form']['error']['signup'],
-            'title'     => ''
-        ); 
-    //$_SESSION['signup_error']= $enJson['form']['error']['signup'];
     header("Location: /index.php?name=Login ");
-    }else{
-        $_SESSION['toastr'] = array(
-            'type'      => 'error', 
-            'message' => "Warning password wrong",
-            'title'     => ''
-        ); 
-
-    //$_SESSION['signup_error']= $enJson['form']['error']['password'];
-    header("Location: /index.php?name=Login ");}
-
 }
+
+
+
+
+
 
--- back-end/actions/RegisterAction.php
+++ back-end/actions/RegisterAction.php
@@ -4,115 +4,121 @@ session_start();
 
 $id = giveId(); //GET ID
 $email = $_POST['email']; // GET MAIL
+// HASH PASSWORD
+$password = hash('sha256', $_POST['password']);
 
 //CHECK IF ALL INPUT HAVE VALUE
-if (empty($_POST['username']) || empty($_POST['password']) || empty($_POST['dateBirth']) || empty($_POST['email'])) {
-    header("Location: /index.php?name=Register ");
-    die();
-}
+if (!empty($_POST['username']) && !empty($_POST['password']) && !empty($_POST['dateBirth']) && !empty($_POST['email'])) {
+        // CHECK IF TYPE OF USERNAME IS VALID
+    if (ctype_alnum($_POST['username'])) {
+        //CHECK IF USERNAME IS NOT ALREADY USE
+        $sql = 'SELECT * FROM user WHERE username = :username ';
+        $query = $db->prepare($sql);
+        $query->execute([
+            ':username' => $_POST["username"],
+        ]);
+        $dataUser = $query->fetch(PDO::FETCH_ASSOC);
+        if (!$dataUser['username']) {
+            // CHECK THE DATEBIRTH AND GET YEARS WITH FUNCTION
+            $dateBirth = explode("-", $_POST['dateBirth']);
+            $newbirth = array_reverse($dateBirth);
+            $yearsUser = HowOldAreYou($newbirth);
+            if ($yearsUser > 13) {
+                //CHECK IS EMAIL IS NOT ALREADY USE
+                $sql = 'SELECT * FROM user WHERE email = :email ';
+                $query = $db->prepare($sql);
 
-// CHECK IF TYPE OF USERNAME IS VALID
-if (!ctype_alnum($_POST['username'])) {
-    
-    // TOASTR ERROR
+                $query->execute([
+                    ':email' => $email,
+                ]);
+                $data = $query->fetch(PDO::FETCH_ASSOC);
+                if (!$data) {
+                   // CHECK IF AVATARE HAVE VALUE ELSE GET DEFAULT AVATAR
+                    if (!empty($_FILES['avatar']['tmp_name'])) {
+                        $tmpName = $_FILES['avatar']['tmp_name'];
+                        $imgData = base64_encode(file_get_contents($tmpName));
+                    } else {
+                        $imgData = "No photo";
+                    }
+                   // INSERT DATA REGISTER IN TABLE USER 
+                    $sql = 'INSERT INTO user(id,email, username, password, profilePicture, birthday,years) VALUES (:id,:email, :pseudo, :password, :photoProfil, :dateBirth,:years)';
+                    $query = $db->prepare($sql);
+                    $query->execute([
+                        ':id' => $id,
+                        ':email' => $email,
+                        ':pseudo' => $_POST['username'],
+                        ':password' => $password,
+                        ':photoProfil' => $imgData,
+                        ':dateBirth' => $_POST['dateBirth'],
+                        ':years' => $yearsUser
+                    ]);
+
+                    // TOASTR SUCCESS
+                    $_SESSION['toastr'] = array(
+                        'type'      => 'success',
+                        'message' => 'Your accompte as be created',
+                        'title'     => ''
+                    );
+
+                    header("Location: /index.php?name=Login");
+                   
+                }else{
+                    $_SESSION['toastr'] = array(
+                        'type'      => 'error',
+                        'message' => 'Your email is already use.',
+                        'title'     => ''
+                    );
+                    header("Location: /index.php?name=Register");
+                }
+            }else{
+                // TOASTR ERROR
+                $_SESSION['toastr'] = array(
+                    'type'      => 'error',
+                    'message' => 'Sorry access to Meta Tube is reserved to the person over 13 years.',
+                    'title'     => ''
+                );
+                header("Location: /index.php?name=Register ");
+                }
+            }else{
+                // TOASTR ERROR
+            $_SESSION['toastr'] = array(
+                'type'      => 'error',
+                'message' => 'This username is already use.',
+                'title'     => ''
+            );
+            header("Location: /index.php?name=Register ");
+        }
+        
+    }else{
+        // TOASTR ERROR
+        $_SESSION['toastr'] = array(
+            'type'      => 'error',
+            'message' => 'Your unsername is not respect the alphanumeric characters.',
+            'title'     => ''
+        );
+
+        header("Location: index.php?name=Register ");
+        
+    }
+}else{
     $_SESSION['toastr'] = array(
         'type'      => 'error',
         'message' => 'Your unsername is not respect the alphanumeric characters.',
         'title'     => ''
     );
-
-    header("Location: index.php?name=Register ");
-    die();
+    header("Location: /index.php?name=Register ");
 }
 
-//CHECK IF USERNAME IS NOT ALREADY USE
-if (!empty($_POST['username'])) {
-    $sql = 'SELECT * FROM user WHERE username = :username ';
-    $query = $db->prepare($sql);
-    $query->execute([
-        ':username' => $_POST["username"],
-    ]);
-    $dataUser = $query->fetch(PDO::FETCH_ASSOC);
-    if ($dataUser['username']) {
-       
-        // TOASTR ERROR
-        $_SESSION['toastr'] = array(
-            'type'      => 'error',
-            'message' => 'This username is already use.',
-            'title'     => ''
-        );
-        header("Location: /index.php?name=Register ");
-        die();
-    }
-}
 
-// CHECK THE DATEBIRTH AND GET YEARS WITH FUNCTION
-if (!empty($_POST['dateBirth'])) {
-    $dateBirth = explode("-", $_POST['dateBirth']);
-    $newbirth = array_reverse($dateBirth);
-    $yearsUser = HowOldAreYou($newbirth);
-    if ($yearsUser < 13) {
 
-        // TOASTR ERROR
-        $_SESSION['toastr'] = array(
-            'type'      => 'error',
-            'message' => 'Sorry access to Meta Tube is reserved to the person over 13 years.',
-            'title'     => ''
-        );
-        header("Location: /index.php?name=Register ");
-        die();
-    }
-}
-// CHECK IF AVATARE HAVE VALUE ELSE GET DEFAULT AVATAR
-if (!empty($_FILES['avatar']['tmp_name'])) {
-    $tmpName = $_FILES['avatar']['tmp_name'];
-    $imgData = base64_encode(file_get_contents($tmpName));
-} else {
-    $imgData = "No photo";
-}
 
-// HASH PASSWORD
-$password = hash('sha256', $_POST['password']);
 
-//CHECK IS EMAIL IS NOT ALREADY USE
-$sql = 'SELECT * FROM user WHERE email = :email ';
-$query = $db->prepare($sql);
 
-$query->execute([
-    ':email' => $email,
-]);
 
-$data = $query->fetch(PDO::FETCH_ASSOC);
-if ($data) {
-    console_log($data);
-    $_SESSION['toastr'] = array(
-        'type'      => 'error',
-        'message' => 'Your email is already use.',
-        'title'     => ''
-    );
 
-    header("Location: /index.php?name=Register");
-    die();
-}
 
-// INSERT DATA REGISTER IN TABLE USER 
-$sql = 'INSERT INTO user(id,email, username, password, profilePicture, birthday,years) VALUES (:id,:email, :pseudo, :password, :photoProfil, :dateBirth,:years)';
-$query = $db->prepare($sql);
-$query->execute([
-    ':id' => $id,
-    ':email' => $email,
-    ':pseudo' => $_POST['username'],
-    ':password' => $password,
-    ':photoProfil' => $imgData,
-    ':dateBirth' => $_POST['dateBirth'],
-    ':years' => $yearsUser
-]);
 
-// TOASTR SUCCESS
-$_SESSION['toastr'] = array(
-    'type'      => 'success',
-    'message' => 'Your accompte as be created',
-    'title'     => ''
-);
 
-header("Location: /index.php?name=Login");
+
+
+
