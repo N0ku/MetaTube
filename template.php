5c42907b9db03132b579bfaf01b8f2d274d1245a
Jerance MICHAEL <michael.jerance@gmail.com>
2022-05-04 16:26:46 +0200

    Fix/ Maxime shit menu Co-authored-by: LucieLecouturier <lucielecouturier2@gmail.com>

--- template.php
+++ template.php
@@ -7,8 +7,6 @@
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
     <script src="./front-end/assets/js/toastr.min.js"></script>
     <script src="./front-end/assets/js/toastr.js.map"></script>
-
-
     <link rel="shortcut icon" href="./front-end/assets/img/favicon.svg" type="image/x-icon">
     <link rel="stylesheet" href="./front-end/assets/css/Global.css">
     <link rel="stylesheet" href="./front-end/assets/css/NavBar.css">
@@ -33,38 +31,51 @@
 </head>
 
 <body>
-    <?= $channelVideoUpload ?>
+  <?= $channelVideoUpload ?>
 
     <?php if ($pageName != "404") {
+        echo $channelCreation;
         echo $navBar;
     } ?>
-    
-    <div class="content">
-        <div class="menus">
+
+<div class="content">
+    <div class="menus">
+            <?php if ($homeBar != "MetaCreator") { ?>
             <?php if ($pageName != "404") {
-                echo $menu;
-            } ?>
+                    echo $menu;
+                } ?>
+        <?php } ?>
+        <?php if($homeBar == "MetaCreator"){ ?>
+
+             <!-- Include here MetaCreator MENU variables for display  °\(°0°)/°-->
+            <?= $menu ?>
+            <?php } ?>
         </div>
         <div class="page-content">
             <div class="page">
+                        <?php if ($homeBar != "MetaCreator") { ?>
+
                 <?php if ($homeBar == "Home") {
-                    echo $filterbar;
-                } ?>
+                        echo $filterbar;
+                    } ?>
                 <?php if ($pageName != "404") {
-                    echo $pageName;
-                } ?>
+                        echo $pageName;
+                    } ?>
+                    <?php } ?>
+                    <?php if($homeBar == "MetaCreator"){ ?>
+                        <?= $pageMeta ?>
+                    <?php } ?>
             </div>
         </div>
     </div>
 
-
-    </div>
     <!-- Add js library and script here -->
 
     <script src="./front-end/assets/js/SearchPage.js"></script>
     <script src="/front-end/assets/js/feature-channel-modal-createCreator.js"></script>
     <script src="/front-end/assets/js/script-menu.js"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.slim.min.js" integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.slim.min.js"
+        integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI=" crossorigin="anonymous"></script>
     <script src="/front-end/assets/js/script-explorer.js"></script>
     <script src="/front-end/assets/js/script-watch.js"></script>
     <script src="./front-end/assets/js/toastr.min.js"></script>
@@ -72,23 +83,23 @@
     <script src="./front-end/assets/js/SearchPage.js"></script>
 
     <script>
-        toastr.options = {
-            "closeButton": true,
-            "debug": false,
-            "newestOnTop": false,
-            "progressBar": true,
-            "positionClass": "toast-bottom-full-width",
-            "preventDuplicates": false,
-            "onclick": null,
-            "showDuration": "300",
-            "hideDuration": "1000",
-            "timeOut": "3000",
-            "extendedTimeOut": "1000",
-            "showEasing": "swing",
-            "hideEasing": "linear",
-            "showMethod": "fadeIn",
-            "hideMethod": "fadeOut"
-        }
+    toastr.options = {
+        "closeButton": true,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": true,
+        "positionClass": "toast-bottom-full-width",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "3000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
     </script>
 
 
