23f6b0f227ff81b7333c0234a50078edc3fccc81
ThomLam <lamiablethomas@gmail.com>
2022-05-04 17:22:09 +0200

    Feature - Add HTML architecture and start CSS in Content

--- front-end/views/pages/MetaCreator/Content.php
+++ front-end/views/pages/MetaCreator/Content.php
@@ -1,17 +1,20 @@
-<?php ob_start();?>
-<div>
+<?php ob_start(); ?>
+<div class="MetaStudio-content-global">
+    <div class="MetaStudio-title">
+        <h1 id="MetaStudio-title-h1">Content of the channel</h1>
+    </div>
+    <div class="MetaStudio-separator-rgb"></div>
 
-<h1 style="color:pink; font-family:Comic Sans MS; margin-top:88px;">CONTENT UwU</h1>
-<h1 style="color:pink; font-family:Comic Sans MS; margin-top:88px;">CONTENT UwU</h1>
-<h1 style="color:pink; font-family:Comic Sans MS; margin-top:88px;">CONTENT UwU</h1>
-<h1 style="color:pink; font-family:Comic Sans MS; margin-top:88px;">CONTENT UwU</h1>
-<h1 style="color:pink; font-family:Comic Sans MS; margin-top:88px;">CONTENT UwU</h1>
-<h1 style="color:pink; font-family:Comic Sans MS; margin-top:88px;">CONTENT UwU</h1>
-<h1 style="color:pink; font-family:Comic Sans MS; margin-top:88px;">CONTENT UwU</h1>
-<h1 style="color:pink; font-family:Comic Sans MS; margin-top:88px;">CONTENT UwU</h1>
-<h1 style="color:pink; font-family:Comic Sans MS; margin-top:88px;">CONTENT UwU</h1>
-<h1 style="color:pink; font-family:Comic Sans MS; margin-top:88px;">CONTENT UwU</h1>
-<h1 style="color:pink; font-family:Comic Sans MS; margin-top:88px;">CONTENT UwU</h1>
-<h1 style="color:pink; font-family:Comic Sans MS; margin-top:88px;">CONTENT UwU</h1>
+    <div class="MetaStudio-selected-category">
+<h1>ntm</h1>
+    </div>
+    <div class="MetaStudio-filter-bar">
+<h2>Bite</h2>
+    </div>
+    <div class="MetaStudio-user-video-config">
+        <h3>Salam</h3>
+    </div>
+
 </div>
+
 <?php $pageMeta = ob_get_clean();?>
--- template.php
+++ template.php
@@ -26,6 +26,7 @@
     <link rel="stylesheet" href="./front-end/assets/css/MetaTubePremium.css">
     <link rel="stylesheet" href="./front-end/assets/css/MetaCreator.css">
     <link rel="stylesheet" href="./front-end/assets/css/MetaCreatorStyle/DashboardStyle.css">
+    <link rel="stylesheet" href="./front-end/assets/css/MetaCreatorStyle/Content.css">
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 
 </head>
