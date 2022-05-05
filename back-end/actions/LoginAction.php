09b226b5300c512c933a6d066564f352a83a74ad
Celiian <celian.opigez@gmail.com>
2022-05-05 13:34:23 +0200

    fix/rebase continue

--- back-end/actions/LoginAction.php
+++ back-end/actions/LoginAction.php
@@ -14,43 +14,41 @@ $query->execute([
 $data = $query->fetch(PDO::FETCH_ASSOC);
 
 
-if( empty($_POST['username']) || empty( $_POST['password'])){
+if (empty($_POST['username']) || empty($_POST['password'])) {
     //$_SESSION['signup_error']= $enJson['form']['error']['signup'] ;
     header("Location: /index.php?name=Login ");
     die();
 }
 
-$password = $_POST['password'];
-
 $password = hash('sha256', $_POST['password']);
 
 
-if ($res["username"] == $data["email"] && $password == $data["password"] ){
-     $_SESSION['user'] = $data;
-     $_SESSION['connect'] = true;
+if ($res["username"] == $data["email"] && $password == $data["password"]) {
+    $_SESSION['user'] = $data;
+    $_SESSION['connect'] = true;
     $_SESSION['toastr'] = array(
-        'type'      => 'success', 
+        'type'      => 'success',
         'message' => "Welcome, {$data['username']}",
         'title'     => ''
-    ); 
+    );
     header('Location: /index.php?name=Home');
-}else{
-    if($res["username"] != $data["email"]){
+} else {
+    if ($res["username"] != $data["email"]) {
         $_SESSION['toastr'] = array(
-            'type'      => 'error', 
+            'type'      => 'error',
             'message' => $enJson['form']['error']['signup'],
             'title'     => ''
-        ); 
-    //$_SESSION['signup_error']= $enJson['form']['error']['signup'];
-    header("Location: /index.php?name=Login ");
-    }else{
+        );
+        //$_SESSION['signup_error']= $enJson['form']['error']['signup'];
+        header("Location: /index.php?name=Login ");
+    } else {
         $_SESSION['toastr'] = array(
-            'type'      => 'error', 
+            'type'      => 'error',
             'message' => "Warning password wrong",
             'title'     => ''
-        ); 
+        );
 
-    //$_SESSION['signup_error']= $enJson['form']['error']['password'];
-    header("Location: /index.php?name=Login ");}
-
+        //$_SESSION['signup_error']= $enJson['form']['error']['password'];
+        header("Location: /index.php?name=Login ");
+    }
 }
