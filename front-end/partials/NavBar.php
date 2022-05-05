9f2e1351a8ad6e29d0a67359f5988d864d1e6dd0
Celiian <celian.opigez@gmail.com>
2022-05-05 14:45:03 +0200

    fix/merge dev

--- front-end/partials/NavBar.php
+++ front-end/partials/NavBar.php
@@ -17,11 +17,18 @@
             </a>
         </div>
 
-        <form method="POST" action="./back-end/actions/filters.php" class="navbar--center">
+        <form method="POST" action="./front-end/views/pages/SearchPage.php" class="navbar--center">
             <input type="text" name="searchBar" class="navbar__center__input" placeholder="<?= $enJson['profile']['navBar']['placeHolderSearch'] ?>" />
-            <button class="navbar__center__search-btn" onclick="window.location.href = 'index.php?name=SearchPage';">
-                <img src="./front-end/assets/img/Logo/search.svg" alt="">
-            </button>
+            <?php if ($homeBar != "SearchPage") { ?>
+                <button class="navbar__center__search-btn" onclick="window.location.href = 'index.php?name=SearchPage';">
+                    <img src="./front-end/assets/img/Logo/search.svg" alt="">
+                </button>
+            <?php } ?>
+            <?php if ($homeBar == "SearchPage") { ?>
+                <button class="navbar__center__search-btn" onclick="Search()">
+                    <img src="./front-end/assets/img/Logo/search.svg" alt="">
+                </button>
+            <?php } ?>
         </form>
 
         <div class="navbar--side">
@@ -75,7 +82,6 @@
                             </label>
                         </div>
                         <!-- LINK TO CHANNEL -->
-
                         <a class="drop--element" onclick="feature_open_accountCreator_function()">
                             <div>
                                 <img src="./front-end/assets/img/Logo/link.svg" alt="">
@@ -126,6 +132,7 @@
     </div>
 </nav>
 <script>
+    << << << < HEAD
     const dropprofil = document.getElementById("dropdown");
 
     function OpenTheDoor() {
@@ -144,7 +151,29 @@
                 }
             }
         }
-    };
+    }; ===
+    === =
+    const dropprofil = document.getElementById("dropdown");
+
+    function OpenTheDoor() {
+        document.getElementById("myDropdown").classList.toggle("show");
+    }
+
+    //Close the dropdown if the user clicks outside of it
+    window.onclick = function(event) {
+        if (!event.target.matches(".dropbtn")) {
+            const dropdowns = document.getElementsByClassName("dropdown-content");
+            var i;
+            for (i = 0; i < dropdowns.length; i++) {
+                const openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains("show")) {
+                    openDropdown.classList.remove("show");
+                }
+            }
+        }
+    }; >>>
+    >>> > 3 a62f77(feature / Search5 add experimental filters.Need to
+        try them.)
 </script>
 
 <?php $navBar = ob_get_clean(); ?>
